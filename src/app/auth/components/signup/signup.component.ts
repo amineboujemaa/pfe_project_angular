bc4d60671d5c6655877ae0110e244edc52d5aa11
amine <mohamedamine.boujemaa@sesame.com.tn>
2024-07-11 20:29:01 +0100

    signup control commit

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -3,56 +3,51 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { AuthService } from '../../services/auth/auth.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-
-
-export class SignupComponent implements OnInit{
-
+export class SignupComponent implements OnInit {
   isSpinning: boolean = false;
   signupForm!: FormGroup;
 
   constructor(
     private fb: FormBuilder,
-    private authService:AuthService,
+    private authService: AuthService,
     private message: NzMessageService, 
-    private router:Router) { }
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.signupForm = this.fb.group({
       name: [null, [Validators.required]],
       email: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required]],
-      checkpassword: [null, [Validators.required, this.confirmationValidate]],
-    })
+      password: [null, [Validators.required, Validators.minLength(8), Validators.pattern(/(?=.*[a-z])(?=.*[A-Z])/)]],
+      checkpassword: [null, [Validators.required, this.confirmationValidate.bind(this)]],
+    });
   }
-  confirmationValidate = (control: FormControl): { [key: string]: boolean } => {
+
+  confirmationValidate(control: FormControl): { [key: string]: boolean } | null {
     if (!control.value) {
       return { required: true };
     } else if (control.value !== this.signupForm.controls['password'].value) {
       return { confirm: true, error: true };
     }
-    return {}; 
-};
+    return null; 
+  }
 
-
-
-
-register(){
-  console.log(this.signupForm.value);
-  this.authService.register(this.signupForm.value).subscribe((res)=>{
-    console.log(res);
-    if(res.id != null){
-      this.message.success('Inscription réussie', { nzDuration: 5000 }); 
-      this.router.navigateByUrl("")
-    }else{
-      this.message.error('Un problème est survenu, verifier vos donner', { nzDuration: 5000 }); 
-
-    }
-  })
+  register() {
+    console.log(this.signupForm.value);
+    this.authService.register(this.signupForm.value).subscribe((res) => {
+      console.log(res);
+      if (res.id != null) {
+        this.message.success('Inscription réussie', { nzDuration: 5000 });
+        this.router.navigateByUrl("");
+      } else {
+        this.message.error('Un problème est survenu, veuillez vérifier vos données', { nzDuration: 5000 });
+      }
+    });
+  }
 }
-}
-
